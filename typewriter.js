ec8ef6a3e8c420a28d21c317cbb69e8dfb4a0069
Taylor Noj <taylornoj@gmail.com>
2021-11-02 17:06:25 +0000

    typewriter completed

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,15 @@
 const sentence = "hello there from lighthouse labs";
-for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, 1000)
-}
+let speed = 50;
+
+for (let i in sentence) {
+  if (i < sentence.length -1) { 
+    setTimeout(() => {
+      process.stdout.write(sentence[i]); // keeps string on one line in node
+    }, speed * i);
+  } else {
+    setTimeout(() => {
+      console.log(sentence[i]); //console.log creates the new line once loop runs through
+    }, speed * i);
+  }
+};
+
